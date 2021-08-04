e454edf9368b59096345b8a2851a5a8d8057b9ac
Cam <camsandybaker@gmail.com>
2021-08-04 17:12:32 -0600

    added headshot

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -24,7 +24,12 @@ const Home = () => {
 
         <div className='row'>
           <div className='col-4'>
-            <h2 className='label'>Who I am</h2>
+            <h2 className='label'>
+              Who I am
+              <img
+                className='headshot'
+                src='https://lh3.googleusercontent.com/I34UsjkcgkTUUuF-TaCCz9hYTZKrDdxQerQxtJAHT-P2_OAgXnRKwDYbsxKEQ5XYrFB3n3NZ0A-tWgR-tU1rUgTWYAzD3jnhmlwtBl0YHMWMdp7gMNB0JmYDBg3bgyv68-OnLvnV-fQJ5yJfbGOwK4rCThVHAI_g29ndfpCPNwKLfxTGdfNs4KCJ98xYSt-YbMQ0zH5dbqhwhHgBUr_SHyuXkcKli4Ui_F2y8Ov-kdyr7lUNGctGOzo4AO26X4TZrpSBhgehFW3T5xuSkKxkstnmSSD8lC6Jx5UO_WvytkbqE2lWE_yGR8d5WBaqNNvp62U_uzi8MySyo0PRcHx3_M56pXu4VtjRp2rN2jLhoSiC_VkKlLQDagxL4EIVs5LAmXKtG3A7jUkWQxfblt_jB_7fxn_hIUdrTgep5vDme7ksCKNxYO1k7HplBYHkxb8d7h--ly9wPH5Ad5o0QzeWDEHuk9vbtOQWQuUNGRxNTtHXs0_sQavjf2ukLPeKMtkks6cZ7jwHzY2TSxnSSTdlQ_qOM_viqbQVFx43MXtYcbncPEB1LsrpX-vpV99gLkwqvGQyZe_2VpoG01ZKke7RuRdhd2PgFmRRhZ4-QyPR9Jd_H2y2DMY8HO8KeM0N4ofZor4V2OFNSCJymfthesDVivkHxe3UmtDg_i4tx-ZNANHc43SPs6anI6KHYqtXDmiRRfRtbvkdML4jmOOZO4nQ5dLu=w686-h914-no?authuser=0'></img>
+            </h2>
           </div>
           <div className='col-6'>
             <p className='paragraph'>
